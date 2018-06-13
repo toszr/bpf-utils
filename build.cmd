0635d01c940f952e6cbadddedb51b299524ae52e
tadamdam <Tomasz.Szreder@gmail.com>
2018-06-13 15:20:04 +0200

    Minor refactoring in build.cmd, more options passed to emcc

--- build.cmd
+++ build.cmd
@@ -3,8 +3,9 @@ setlocal
 set "SCRIPT_DIR=%~dp0"
 
 set "BUILD_OUTPUT_DIR=%SCRIPT_DIR%build"
+set "OPTIMIZATION_LEVEL=-Oz --memory-init-file 0"
 
-set "OUTFILE=%BUILD_OUTPUT_DIR%\bpf-test.html"
+set "OUTFILE=%BUILD_OUTPUT_DIR%\bpf-test.js"
 set "INFILE=%SCRIPT_DIR%bpf-test.c"
 
 set "WPCAP_LIBPCAP_SRC_DIR=%SCRIPT_DIR%winpcap\wpcap\libpcap"
@@ -16,7 +17,9 @@ if not exist "%BUILD_OUTPUT_DIR%" mkdir "%BUILD_OUTPUT_DIR%"
 REM Transpile the libpcap C code (well, parts of it) 
 REM to js or html (based on outfile extension)
 pushd "%WPCAP_LIBPCAP_SRC_DIR%"
-emcc "%INFILE%" %DEPENDENCIES% -I"%WPCAP_LIBPCAP_SRC_DIR%" -DINET6 -D_U_="" -o "%OUTFILE%" 
+emcc "%INFILE%" %DEPENDENCIES% %OPTIMIZATION_LEVEL% -I"%WPCAP_LIBPCAP_SRC_DIR%" -DINET6 -D_U_="" -o "%OUTFILE%" ^
+	-s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'Pointer_stringify']" ^
+	-s WASM=1 -g
 popd 
 
 
